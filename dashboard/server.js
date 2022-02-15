324209929e86f04ca9e51b7ed6b37927f7ab2bd0
itamar1238 <thrillerface9@gmail.com>
2022-02-15 23:05:12 +0000

    Updates

--- dashboard/server.js
+++ dashboard/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express()
 const port = 3000
-
+const { commands } = require('../handlers/command-handler')
 app.set('views', __dirname + '/views')
 app.set('view engine', 'pug')
 
@@ -11,6 +11,10 @@ app.locals.basedir = `${__dirname}/assets`;
 app.get('/', (req, res) => {
     res.render('index');
 })
-app.get('/commands', (req,res) => res.render('commands'))
+app.get('/commands', (req,res) => res.render('commands', {
+    subtitle: 'Commands',
+    categories: [{name: 'Auto Mod', icon: "fas fa-gavel"}, {name: 'Economy', icon: "fas fa-coins"}, {name: 'General', icon: "fa-solid fa-browser"}, {name: 'Music', icon: "fas fa-music"}],
+    commands: Array.from(commands.values())
+}))
 
 app.listen(port, () => console.log(`Server is live on port ${port}`))
